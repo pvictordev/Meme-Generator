169ca59e063e907837d8b012950090539b179ee9
Purice Victor <pvictor0110@gmail.com>
2023-09-04 21:51:17 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import viteLogo from '/vite.svg'
 import React from 'react'; 
 import './App.css'
 import Header from './components/Header';
+import Meme from './components/Meme';
+
+
 
 function App() {
   return (
     <>
       <Header/>
+      <Meme/>
+      
     </>
   )
 }
