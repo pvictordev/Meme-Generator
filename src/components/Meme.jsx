61d5dff3a3f1d5220784b0a201ff26e8c19b44f6
Purice Victor <pvictor0110@gmail.com>
2023-09-11 15:32:50 +0300

    commit

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,17 @@
-import React from "react"
+import React, {useState} from "react"
 import memesData from "../memesData"
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        
+    }
+        
     return (
         <main>
             <div className="form">
@@ -17,11 +27,13 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-                
+         
             </div>
+            <img className="meme--image"src={memeImage} alt="" />
         </main>
     )
 }
